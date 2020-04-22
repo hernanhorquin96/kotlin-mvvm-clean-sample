6be77a36732434197fe221bb58bbafed15bd72f7
Hernan Horquín <hernan.horquin@globant.com>
2020-04-22 11:33:48 -0300

    Show character data in dialog fragment

--- app/src/main/java/com/globant/di/KoinModules.kt
+++ app/src/main/java/com/globant/di/KoinModules.kt
@@ -1,5 +1,6 @@
 package com.globant.di
 
+import com.globant.viewmodels.CharacterDetailViewModel
 import com.globant.viewmodels.CharacterViewModel
 import com.globant.viewmodels.CharactersViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -8,4 +9,5 @@ import org.koin.dsl.module
 val viewModelsModule = module {
     viewModel { CharacterViewModel(get()) }
     viewModel { CharactersViewModel(get()) }
+    viewModel { CharacterDetailViewModel(get()) }
 }
--- app/src/main/java/com/globant/utils/Data.kt
+++ app/src/main/java/com/globant/utils/Data.kt
@@ -5,4 +5,4 @@ package com.globant.utils
  */
 data class Data<RequestData>(var responseType: Status, var data: RequestData? = null, var error: Exception? = null)
 
-enum class Status { SUCCESSFUL, ERROR, LOADING, GetCharacterSuccess, GetCharacterError }
+enum class Status { SUCCESSFUL, ERROR, LOADING, GetCharacterSuccess, GetCharacterError, GetCharacterByIdSuccess, GetCharacterByIdError }
